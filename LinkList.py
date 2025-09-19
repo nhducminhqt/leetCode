c51bf78ee0405f6ced797c0cd4c3173a60b946da
nhducminhqt <nhducminhqt@gmail.com>
2025-09-19 10:00:00 +0700

    add

--- LinkList.py
+++ LinkList.py
@@ -1,13 +1,14 @@
-class ListNode(object):
-    def __init__(self,val=0,next=None):
-        self.val = val
-        self.next = next
-class Solution(object):
-    def DD(self,head):
-        p = head
-        while p.next:
-            if p.next.val == p.val:
-                p.next = p.next.next
-            else:
-                p=p.next
-        return head
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = Solution().twoSum(nums, target)
+    print(result) 
